f1cce552615614ab92e4e9431656e4c7388daf56
Mike Knoop <mikeknoop@gmail.com>
2012-09-29 18:50:54 -0500

    add some flatpages

--- prlibs.py
+++ prlibs.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flaskext.flatpages import FlatPages
+
+FLATPAGES_EXTENSION = '.html'
+
 app = Flask(__name__)
+pages = FlatPages(app)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return pages.get_or_404('static/index').html
+
+@app.route('/<path:path>/')
+def page(path):
+    return pages.get_or_404('static/'+path).html
 
 if __name__ == "__main__":
     app.run()
