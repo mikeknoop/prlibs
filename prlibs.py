e965dcc7264898f2c74a44117828a04e56a000ba
Mike Knoop <mike@knoopgroup.com>
2012-09-29 19:02:27 -0500

    ignore pyc

--- prlibs.py
+++ prlibs.py
@@ -1,18 +1,22 @@
 from flask import Flask
+app = Flask(__name__)
 from flaskext.flatpages import FlatPages
 
-FLATPAGES_EXTENSION = '.html'
+#DEBUG = True
+#FLATPAGES_AUTO_RELOAD = DEBUG
+#FLATPAGES_EXTENSION = '.html'
 
-app = Flask(__name__)
-pages = FlatPages(app)
+#app = Flask(__name__)
+#pages = FlatPages(app)
 
 @app.route("/")
 def index():
-    return pages.get_or_404('static/index').html
+     return "Hello World"
+#    return pages.get_or_404('static/index').html
 
-@app.route('/<path:path>/')
-def page(path):
-    return pages.get_or_404('static/'+path).html
+#@app.route('/<path:path>/')
+#def page(path):
+#    return pages.get_or_404('static/'+path).html
 
 if __name__ == "__main__":
     app.run()
