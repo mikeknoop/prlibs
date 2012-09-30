d568631a5eb5f9af1f1ad22033aab1e50539aebd
Mike Knoop <mikeknoop@gmail.com>
2012-09-29 19:06:56 -0500

    add flatpage

--- prlibs.py
+++ prlibs.py
@@ -1,21 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
 from flask_flatpages  import FlatPages
 
-#DEBUG = True
-#FLATPAGES_AUTO_RELOAD = DEBUG
-#FLATPAGES_EXTENSION = '.html'
+FLATPAGES_EXTENSION = '.html'
 
-#pages = FlatPages(app)
+app = Flask(__name__)
+pages = FlatPages(app)
 
 @app.route("/")
 def index():
-     return "Hello World"
-#    return pages.get_or_404('static/index').html
+   return pages.get_or_404('static/index').html
 
-#@app.route('/<path:path>/')
-#def page(path):
-#    return pages.get_or_404('static/'+path).html
+@app.route('/<path:path>/')
+def page(path):
+   return pages.get_or_404('static/'+path).html
 
 if __name__ == "__main__":
-    app.run()
+   app.run()
