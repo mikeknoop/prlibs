eeb60c7e3db5821f0049755fceff71f1f0493b69
Mike Knoop <mikeknoop@gmail.com>
2012-09-30 02:06:21 -0500

    semantically parse urls

--- prlibs.py
+++ prlibs.py
@@ -13,5 +13,21 @@ def index():
 def generate():
    return pages.get_or_404('success').body
 
+@app.route('/test')
+def test():
+   import requests
+   import nltk
+   import lxml
+   from lxml.html.clean import clean_html
+   r = requests.get('https://zapier.com/blog')
+   t = lxml.html.fromstring(r.content)
+   t = clean_html(t)
+   text = t.text_content()
+   text = text.encode('utf-8')
+   tokens = nltk.word_tokenize(text)
+   t = nltk.Text(tokens)
+   tags = nltk.pos_tag(t)
+   return '[%s]' % ', '.join(map(str, tags))
+
 if __name__ == "__main__":
-   app.run()
+   app.run(host='192.168.56.10')
